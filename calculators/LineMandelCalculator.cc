11f7ceab9236328077d9178d24a992b651d71ab8
Jana Brandejsova <jana.brandejsova@gmail.com>
2025-11-02 12:03:54 +0100

    Line implemented - 2,69 GFLOPS, 0,98 GINTOPS

--- calculators/LineMandelCalculator.cc
+++ calculators/LineMandelCalculator.cc
@@ -19,14 +19,52 @@ LineMandelCalculator::LineMandelCalculator (unsigned matrixBaseSize, unsigned li
     BaseMandelCalculator(matrixBaseSize, limit, "LineMandelCalculator")
 {
     // @TODO allocate & prefill memory
+    data = (int *)(malloc(height * width * sizeof(int)));
 }
 
 LineMandelCalculator::~LineMandelCalculator() {
     // @TODO cleanup the memory
+    free(data);
+    data = NULL;
 }
 
 
 int * LineMandelCalculator::calculateMandelbrot () {
     // @TODO implement the calculator & return array of integers
-    return NULL;
+
+    int *pdata = data;
+
+    for (int i = 0; i < height; ++i)
+    {
+        const float y = y_start + i * dy;
+
+        // Vektorizovaná vnitřní smyčka přes sloupce (SIMD-friendly)
+        #pragma omp simd aligned(pdata:64)
+        for (int j = 0; j < width; ++j)
+        {
+            const float x = x_start + j * dx;
+
+            // Výpočet Mandelbrotu přímo zde, bez volání mandelbrot()
+            float zReal = 0.0f;
+            float zImag = 0.0f;
+            int iter = 0;
+
+            for (; iter < limit; ++iter)
+            {
+                const float r2 = zReal * zReal;
+                const float i2 = zImag * zImag;
+
+                if (r2 + i2 > 4.0f)
+                    break;
+
+                const float zrTemp = r2 - i2 + x;
+                zImag = 2.0f * zReal * zImag + y;
+                zReal = zrTemp;
+            }
+
+            pdata[i * width + j] = iter;
+        }
+    }
+
+    return data;
 }
--- calculators/LineMandelCalculator.h
+++ calculators/LineMandelCalculator.h
@@ -16,4 +16,6 @@ public:
 
 private:
     // @TODO add all internal parameters
+    int *data;
+
 };
